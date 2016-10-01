5e6ead273cf7dd058f0cb894fac198b7f18bed7e
ElijahButers <Elijahbuters@gmail.com>
2016-10-01 18:10:20 +0300

    Added a property that holds a CLLocationManager that lets us configure how we want to be notified about location, and will also deliver location updates to us.

--- DetectABeacon.Project22/ViewController.swift
+++ DetectABeacon.Project22/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var distanceReading: UILabel!
     
+    var locationManager: CLLocationManager!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
